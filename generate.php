621fc133f79aead6336e198071e0bc8eb49e02df
Richard Cyganiak <richard@cyganiak.de>
2010-10-08 14:03:23 +0100

    check in current code

---
+++ generate.php
@@ -0,0 +1,258 @@
+<?php
+
+//$debug_maxdatasets = 5;
+$base = 'http://lod-cloud.net/';
+$filename = 'void.ttl';
+$fileurl = $base . 'data/' . $filename;
+
+@ini_set('error_reporting', E_ALL);
+
+include_once('Ckan_client-PHP/Ckan_client.php');
+
+// Fetching datasets from CKAN API
+$ckan = new Ckan_client();
+echo "Fetching dataset list ... ";
+$group_description = $ckan->get_group_entity('lodcloud');
+echo "OK (" . count($group_description->packages) . " datasets)\n";
+$datasets = array();
+foreach ($group_description->packages as $package_id) {
+  if (@$debug_maxdatasets && count($datasets) == $debug_maxdatasets) break;
+  echo "Fetching dataset $package_id ... ";
+  $package = $ckan->get_package_entity($package_id);
+  echo $package->name . "\n";
+  $datasets[$package->name] = $package;
+}
+
+// Extracting linkset info from custom fields
+echo "Calculating linksets ... ";
+$linkset_count = 0;
+foreach ($datasets as $package => $details) {
+  $datasets[$package]->inlinks = array();
+}
+foreach ($datasets as $package => $details) {
+  $datasets[$package]->outlinks = array();
+  $extras = get_object_vars($details->extras);
+  foreach ($extras as $key => $value) {
+    if (!preg_match('/^links:(.*)/', $key, $match)) continue;
+    $target = $match[1];
+    if (!array_key_exists($target, $datasets)) continue;
+    $datasets[$package]->outlinks[$target] = (int) $value;
+    $datasets[$target]->inlinks[$package] = (int) $value;
+    $linkset_count++;
+  }
+  ksort($datasets[$package]->outlinks);
+}
+foreach (array_keys($datasets) as $package) {
+  ksort($datasets[$package]->inlinks);
+}
+echo "OK ($linkset_count linksets)\n";
+
+// Inspect resources to identify SPARQL endpoints, dumps, examples etc
+echo "Categorising resources ... ";
+foreach ($datasets as $package => $dataset) {
+  $datasets[$package]->examples = array();
+  $datasets[$package]->dumps = array();
+  $datasets[$package]->other_resources = array();
+  foreach ($dataset->resources as $resource) {
+    $url = @trim($resource->url);
+    if (!preg_match('!^(https?|ftp):\/\/[^<> ]*$!', $url)) continue; 
+    $format = strtolower(trim($resource->format));
+    $description = @$resource->description;
+    if ($format == 'api/sparql') {
+      $datasets[$package]->sparql = $url;
+    } else if ($format == 'example/rdf+xml') {
+      $datasets[$package]->examples[] = array($url, 'application/rdf+xml', $description);
+    } else if ($format == 'example/turtle') {
+      $datasets[$package]->examples[] = array($url, 'text/turtle', $description);
+    } else if ($format == 'example/ntriples') {
+      $datasets[$package]->examples[] = array($url, 'application/x-ntriples', $description);
+    } else if ($format == 'example/rdfa') {
+      $datasets[$package]->examples[] = array($url, 'text/html', $description);
+    } else if ($format == 'application/rdf+xml' || $format == 'text/turtle' || $format == 'application/x-ntriples' || $format == 'application/x-nquads') {
+      $datasets[$package]->dumps[] = array($url, $format, $description);
+    } else {
+      $datasets[$package]->other_resources[] = array($url, $format, $description);
+    }
+  }
+}
+echo "OK\n";
+
+echo "Fetching license list ... ";
+$ckan_licenses = $ckan->get_license_list();
+$licenses = array();
+foreach ($ckan_licenses as $license) {
+  $licenses[$license->id] = $license;
+}
+echo "OK (" . count($licenses) . " licenses)\n";
+
+// Themes
+$themes = array(
+  'media' => array('Media'),
+  'geographic' => array('Geography'),
+  'lifesciences' => array('Life sciences'),
+  'publications' => array('Publications', 'Including library and museum data'),
+  'government' => array('Government'),
+  'ecommerce' => array('eCommerce'),
+  'socialweb' => array('Social Web', 'People and their activities'),
+  'usergeneratedcontent' => array('User-generated content', 'Blog posts, discussions, pictures, etc.'),
+  'schemata' => array('Schemata', 'Structural resources, including vocabularies, ontologies, classifications, thesauri'),
+  'crossdomain' => array('Cross-domain'),
+);
+
+// Prepare RDF writer
+include_once('rdfwriter.inc.php');
+$out = new NeologismRDFWriter();
+$out->register_namespace('void', '');
+$out->register_namespace('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+$out->register_namespace('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
+$out->register_namespace('owl', 'http://www.w3.org/2002/07/owl#');
+$out->register_namespace('xsd', 'http://www.w3.org/2001/XMLSchema#');
+$out->register_namespace('foaf', 'http://xmlns.com/foaf/0.1/');
+$out->register_namespace('dcterms', 'http://purl.org/dc/terms/');
+$out->register_namespace('dbp', 'http://dbpedia.org/property/');
+$out->register_namespace('void', 'http://rdfs.org/ns/void#');
+$out->register_namespace('tag', 'http://www.holygoat.co.uk/owl/redwood/0.1/tags/');
+$out->register_namespace('skos', 'http://www.w3.org/2004/02/skos/core#');
+$out->register_namespace('ov', 'http://open.vocab.org/terms/');
+
+// Create RDF metadata about the RDF document itself
+$richard = 'http://richard.cyganiak.de/#me';
+$out->triple_literal($fileurl, 'dcterms:title', 'The LOD Cloud diagram in RDF');
+$out->triple_literal($fileurl, 'dcterms:description', 'This file contains RDF descriptions of all RDF datasets in the LOD Cloud diagram, generated from metadata in the lodcloud group in CKAN, expressed using the voiD vocabulary.');
+$out->triple_literal($fileurl, 'dcterms:modified', date('c'), 'xsd:dateTime');
+$out->triple_uri($fileurl, 'dcterms:creator', $richard);
+$out->triple_uri($fileurl, 'dcterms:license', 'http://creativecommons.org/publicdomain/zero/1.0/');
+$out->triple_uri($fileurl, 'dcterms:source', 'http://ckan.net/group/lodcloud');
+$out->triple_uri($fileurl, 'rdfs:seeAlso', $base);
+$out->triple_uri($fileurl, 'rdfs:seeAlso', 'http://rdfs.org/ns/void-guide');
+$out->triple_uri($fileurl, 'foaf:depiction', 'http://richard.cyganiak.de/2007/10/lod/lod-datasets_2010-09-22.png');
+
+// Create RDF information about Richard
+$out->triple_qname($richard, 'a', 'foaf:Person');
+$out->triple_literal($richard, 'foaf:name', 'Richard Cyganiak');
+$out->triple_uri($richard, 'foaf:homepage', 'http://richard.cyganiak.de/');
+$out->triple_uri($richard, 'foaf:mbox', 'mailto:richard@cyganiak.de');
+
+// Create RDF information about themes
+$scheme_uri = $base . 'themes';
+$out->triple_qname($scheme_uri, 'a', 'skos:ConceptScheme');
+$out->triple_literal($scheme_uri, 'skos:prefLabel', 'LOD Cloud Themes');
+foreach ($themes as $id => $details) {
+  $theme_uri = $scheme_uri . '/' . $id;
+  $out->triple_qname($theme_uri, 'a', 'skos:Concept');
+  $out->triple_literal($theme_uri, 'skos:prefLabel', $details[0]);
+  $out->triple_literal($theme_uri, 'skos:scopeNote', @$details[1]);
+  $out->triple_uri($theme_uri, 'skos:inScheme', $scheme_uri);
+}
+
+// Create RDF information about licenses
+foreach ($licenses as $id => $details) {
+  $out->triple_literal($base . 'license/' . $id, 'rdfs:label', $details->title);
+  $out->triple_literal($base . 'license/' . $id, 'foaf:page', $details->url);
+}
+
+// Create RDF information about each dataset
+foreach ($datasets as $key => $dataset) {
+  $ds = $base . $key;
+  $out->triple_qname($ds, 'a', 'void:Dataset');
+  $out->triple_literal($ds, 'dcterms:title', $dataset->title);
+  if (isset($dataset->extras->shortname)) {
+    $out->triple_literal($ds, 'skos:altLabel', $dataset->extras->shortname);
+  }
+  $out->triple_literal($ds, 'dcterms:description', $dataset->notes);
+  $out->triple_uri($ds, 'foaf:homepage', $dataset->url);
+  $out->triple_uri($ds, 'foaf:page', $dataset->ckan_url);
+
+  // Licenses ... Work around broken data for RKB datasets
+  if ($dataset->license_id && !preg_match('/ /', $dataset->license_id) && $dataset->license_id != 'None') {
+    $out->triple_uri($ds, 'dcterms:license', $base . 'license/' . $dataset->license_id);
+  }
+  if (isset($dataset->extras->license_link)) {
+    $out->triple_uri($ds, 'dcterms:license', $dataset->extras->license_link);
+  }
+
+  if (isset($dataset->extras->triples)) {
+    $out->triple_literal($ds, 'void:triples', $dataset->extras->triples, 'xsd:integer');
+  }
+  // Resources
+  if (isset($dataset->sparql)) {
+    $out->triple_uri($ds, 'void:sparqlEndpoint', $dataset->sparql);
+  }
+  foreach ($dataset->dumps as $dump) {
+    $out->triple_uri($ds, 'void:dataDump', $dump[0]);
+  }
+  foreach ($dataset->examples as $example) {
+    $out->triple_uri($ds, 'void:exampleResource', $example[0]);
+  }
+  foreach ($dataset->other_resources as $resource) {
+    $out->triple_uri($ds, 'dcterms:relation', $resource[0]);
+  }
+  if ($dataset->ratings_count) {
+    $out->triple_literal($ds, 'ov:ratings_count', $dataset->ratings_count, 'xsd:integer');
+    $out->triple_literal($ds, 'ov:ratings_average', $dataset->ratings_average, 'xsd:decimal');
+  }
+  // Author and maintainer
+  $out->triple_uri($ds, 'dcterms:contributor', $dataset->author ? ($ds . '/author') : null);
+  $out->triple_uri($ds, 'dcterms:contributor', $dataset->maintainer ? ($ds . '/maintainer') : null);
+  // Linksets
+  foreach ($dataset->outlinks as $target => $link_count) {
+    $out->triple_uri($ds, 'void:subset', $ds . '/links/' . $target);
+  }
+  // Themes
+  foreach ($dataset->tags as $tag) {
+    if (!isset($themes[$tag])) continue;
+    $out->triple_uri($ds, 'dcterms:subject', $base . 'themes/' . $tag);
+  }
+  // Tags
+  foreach ($dataset->tags as $tag) {
+    $out->triple_uri($ds, 'tag:taggedWithTag', $base . 'tag/' . $tag);
+  }
+  // Author details
+  if ($dataset->author) {
+    $out->triple_literal($ds . '/author', 'rdfs:label', $dataset->author);
+    if ($dataset->author_email) {
+      if (preg_match('!^http://!', $dataset->author_email)) {
+        $out->triple_uri($ds . '/author', 'foaf:homepage', $dataset->author_email);
+      } else if (preg_match('/^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/', $dataset->author_email)) {
+        $out->triple_uri($ds . '/author', 'foaf:mbox', 'mailto:' . $dataset->author_email);
+      }
+    }
+  }
+  // Maintainer details
+  if ($dataset->maintainer) {
+    $out->triple_literal($ds . '/maintainer', 'rdfs:label', $dataset->maintainer);
+    if ($dataset->maintainer_email) {
+      if (preg_match('!^http://!', $dataset->maintainer_email)) {
+        $out->triple_uri($ds . '/maintainer', 'foaf:homepage', $dataset->maintainer_email);
+      } else if (preg_match('/^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/', $dataset->maintainer_email)) {
+        $out->triple_uri($ds . '/maintainer', 'foaf:mbox', 'mailto:' . $dataset->maintainer_email);
+      }
+    }
+  }
+  // Linkset details
+  foreach ($dataset->outlinks as $target => $link_count) {
+    $ls = $ds . '/links/' . $target;
+    $out->triple_qname($ls, 'a', 'void:Linkset');
+    $out->triple_uri($ls, 'void:target', $ds);
+    $out->triple_uri($ls, 'void:target', $base . $target);
+    $out->triple_literal($ls, 'void:triples', $link_count, 'xsd:integer');
+  }
+  // Resource details (same structure for all kinds of resources)
+  $resources = array_merge($dataset->dumps, $dataset->examples, $dataset->other_resources);
+  foreach ($resources as $details) {
+    list($url, $format, $description) = $details;
+    $out->triple_literal($url, "dcterms:description", $description);
+    $out->triple_literal($url, "dcterms:format", $format);
+  }
+}
+
+// Serialize to Turtle
+$ser = new NeologismTurtleSerializer($out->_namespaces);
+$rdf = $ser->getSerializedTriples($out->_triples);
+
+// Write to file
+echo "Writing to file $filename ... ";
+$file = fopen($filename, 'w');
+fputs($file, $rdf);
+fclose($file);
+echo "OK\n";
---
+++ rdfwriter.inc.php
@@ -0,0 +1,286 @@
+<?php
+
+include_once('arc/ARC2.php');
+ARC2::inc('TurtleSerializer');
+ARC2::inc('RDFXMLSerializer');
+
+/**
+ * A writer for RDF statements. Encapsulates RAP storage and serialisation,
+ * has its own namespace management, and provides a convenient API for
+ * generating RDF statements.
+ */
+class NeologismRDFWriter {
+  var $_namespaces = array();
+  var $_triples = array();
+
+  /**
+   * Registers a namespace mapping that will be added to the written
+   * RDF file.
+   *
+   * @param $prefix
+   *   A namespace prefix, such as 'foaf'
+   * @param $uri
+   *   A namespace URI, such as 'http://xmlns.com/foaf/0.1/'
+   */
+  function register_namespace($prefix, $uri) {
+    if (in_array($uri, $this->_namespaces)) return;
+    $this->_namespaces[$prefix] = $uri;
+  }
+
+  function triple_literal($s, $p, $o, $type = null) {
+    $this->_triple($s, $p, $o, 'literal', $this->_expand_qname($type));
+  }
+
+  function triple_uri($s, $p, $o) {
+    $this->_triple($s, $p, $o, 'uri');
+  }
+
+  function triple_qname($s, $p, $o) {
+    $this->_triple($s, $p, $this->_expand_qname($o), 'uri');
+  }
+
+  function triples_qname($s, $p, $os) {
+    if (empty($os)) return;
+    if (!is_array($os)) {
+      throw new Exception("Not an array: '$os'");
+    }
+    foreach ($os as $qname) {
+      $this->triple_qname($s, $p, $qname);
+    }
+  }
+
+  function _triple($s, $p, $o, $o_type, $datatype = null) {
+    if (empty($s) || empty($p) || empty($o)) return;
+    $p = ($p == 'a') ? 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' : $this->_expand_qname($p);
+    if (!$p) return;
+    $this->_triples[] = array('s' => $s, 's_type' => 'uri', 'p' => $p, 'o' => $o, 'o_type' => $o_type, 'o_datatype' => $datatype);
+  }
+
+  function _expand_qname($qname) {
+    if ($qname == null) return null;
+    list($prefix, $local) = explode(':', $qname);
+    if (!isset($this->_namespaces[$prefix])) return null;
+    return $this->_namespaces[$prefix] . $local;
+  }
+
+  function write_rdfxml() {
+    drupal_set_header("Content-Type: application/rdf+xml; charset=utf-8");
+    module_load_include('inc', 'neologism', 'neologism.arc');
+    $ser = ARC2::getRDFXMLSerializer(array('ns' => $this->_namespaces, 'serializer_type_nodes' => true));
+    echo $ser->getSerializedTriples($this->_triples);
+  }
+
+  function write_n3() {
+    drupal_set_header("Content-Type: text/rdf+n3; charset=utf-8");
+    module_load_include('inc', 'neologism', 'neologism.arc');
+    $ser = new NeologismTurtleSerializer($this->_namespaces);
+    echo $ser->getSerializedTriples($this->_triples);
+  }
+}
+
+/**
+ * A customized Turtle serializer, based on the one inculded in ARC2.
+ * It uses slightly different rules for compressing URIs into QNames,
+ * to make more pleasing output. We keep it in a separate include file
+ * to avoid having to include ARC2.php all the time.
+ */
+class NeologismTurtleSerializer extends ARC2_TurtleSerializer {
+
+  function __construct($namespaces) {
+    parent::__construct(array('ns' => $namespaces), new stdClass());
+  }
+
+  function getTerm($v, $term = '', $qualifier = '') {
+    if ($v === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' && $term === 'p') return 'a';
+    if (!is_array($v) && ($term === 's' || $term === 'o' || $term === 'dt')) {
+      foreach ($this->ns as $prefix => $uri) {
+        if (strpos($v, $uri) === 0) {
+          $local = substr($v, strlen($uri));
+          if (preg_match('/^[a-z0-9_][a-z0-9_-]*$/i', $local)) {
+            if (!in_array($uri, $this->used_ns)) $this->used_ns[] = $uri;
+            return $prefix . ':' . $local;
+          }
+        }
+      }
+    }
+    if (is_array($v) && @$v['type'] == 'literal') {
+      if (isset($v['datatype'])
+        && $v['datatype'] == 'http://www.w3.org/2001/XMLSchema#integer'
+        && (is_int($v['value']) || preg_match('/^[-+0-9]+$/', $v['value']))) return (string)$v['value'];
+      if (isset($v['datatype'])
+        && $v['datatype'] == 'http://www.w3.org/2001/XMLSchema#decimal'
+        && (is_numeric($v['value']) || preg_match('/^[-+e0-9.]+$/', $v['value']))) return (string)$v['value'];
+      $suffix = isset($v['lang']) && $v['lang'] ? '@' . $v['lang'] : '';
+      $suffix = isset($v['datatype']) && $v['datatype'] ? '^^' . $this->getTerm($v['datatype'], 'dt') : $suffix;
+      return NeologismTurtleSerializer::quote($v['value']) . $suffix;
+    }
+    return parent::getTerm($v, $term, $qualifier);
+  }
+  
+  function getSerializedIndex($index, $raw = 0) {
+    $r = '';
+    $nl = "\n";
+    foreach ($index as $s => $ps) {
+      $r .= $r ? ' .' . $nl . $nl : '';
+      $s = $this->getTerm($s, 's');
+      $r .= $s;
+      $first_p = 1;
+      foreach ($ps as $p => $os) {
+        if (!$os) continue;
+        $p = $this->getTerm($p, 'p');
+        $r .= $p === 'a' ? ' ' : ($first_p ? '' : ';') . $nl . '    ';
+        $r .= $p;
+        $first_o = 1;
+        if (!is_array($os)) {/* single literal o */
+          $os = array(array('value' => $os, 'type' => 'literal'));
+        }
+        if (count($os) == 1) {
+          $r .= ' ' . $this->getTerm($os[0], 'o', $p);
+        } else {
+          foreach ($os as $o) {
+            $r .= $p === 'a' ? ($first_o ? ' ' : ', ') : ($first_o ? '' : ',') . $nl . '        ';
+            $o = $this->getTerm($o, 'o', $p);
+            $r .= $o;
+            $first_o = 0;
+          }
+        }
+        $first_p = 0;
+      }
+    }
+    $r .= $r ? ' .' . $nl : '';
+    if ($raw) {
+      return $r;
+    }
+    return $r ? $this->getHead() . $nl . $nl . $r : '';
+  }
+
+  private static function quote($str) {
+    if (preg_match("/[\\x0A\\x0D]/", $str)) {
+      return '"""' . preg_replace_callback(
+          "/[\\x00-\\x09\\x0B\\x0C\\x0E-\\x1F\\x22\\x5C\\x7F]|[\\x80-\\xBF]|[\\xC0-\\xFF][\\x80-\\xBF]*/",
+          array('NeologismTurtleSerializer', 'escape_callback'),
+          $str) . '"""';
+    } else {
+      return '"' . NeologismTurtleSerializer::escape($str) . '"';
+    }
+  }
+
+  const error_character = '\\uFFFD';
+
+  // Input is an UTF-8 encoded string. Output is the string in N-Triples encoding.
+  // Checks for invalid UTF-8 byte sequences and replaces them with \uFFFD (white
+  // question mark inside black diamond character)
+  //
+  // Sources:
+  // http://www.w3.org/TR/rdf-testcases/#ntrip_strings
+  // http://en.wikipedia.org/wiki/UTF-8
+  // http://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt
+  private static function escape($str) {
+    // Replaces all byte sequences that need escaping. Characters that can
+    // remain unencoded in N-Triples are not touched by the regex. The
+    // replaced sequences are:
+    //
+    // 0x00-0x1F   non-printable characters
+    // 0x22        double quote (")
+    // 0x5C        backslash (\)
+    // 0x7F        non-printable character (Control)
+    // 0x80-0xBF   unexpected continuation byte, 
+    // 0xC0-0xFF   first byte of multi-byte character,
+    //             followed by one or more continuation byte (0x80-0xBF)
+    //
+    // The regex accepts multi-byte sequences that don't have the correct
+    // number of continuation bytes (0x80-0xBF). This is handled by the
+    // callback.
+    return preg_replace_callback(
+        "/[\\x00-\\x1F\\x22\\x5C\\x7F]|[\\x80-\\xBF]|[\\xC0-\\xFF][\\x80-\\xBF]*/",
+        array('NeologismTurtleSerializer', 'escape_callback'),
+        $str);
+  }
+
+  private static function escape_callback($matches) {
+    $encoded_character = $matches[0];
+    $byte = ord($encoded_character[0]);
+    // Single-byte characters (0xxxxxxx, hex 00-7E)
+    if ($byte == 0x09) return "\\t";
+    if ($byte == 0x0A) return "\\n";
+    if ($byte == 0x0D) return "\\r";
+    if ($byte == 0x22) return "\\\"";
+    if ($byte == 0x5C) return "\\\\";
+    if ($byte < 0x20 || $byte == 0x7F) {
+      // encode as \u00XX
+      return "\\u00" . sprintf("%02X", $byte);
+    }
+    // Multi-byte characters
+    if ($byte < 0xC0) {
+      // Continuation bytes (0x80-0xBF) are not allowed to appear as first byte
+      return NeologismTurtleSerializer::error_character;
+    }
+    if ($byte < 0xE0) { // 110xxxxx, hex C0-DF
+      $bytes = 2;
+      $codepoint = $byte & 0x1F;
+    } else if ($byte < 0xF0) { // 1110xxxx, hex E0-EF
+      $bytes = 3;
+      $codepoint = $byte & 0x0F;
+    } else if ($byte < 0xF8) { // 11110xxx, hex F0-F7
+      $bytes = 4;
+      $codepoint = $byte & 0x07;
+    } else if ($byte < 0xFC) { // 111110xx, hex F8-FB
+      $bytes = 5;
+      $codepoint = $byte & 0x03;
+    } else if ($byte < 0xFE) { // 1111110x, hex FC-FD
+      $bytes = 6;
+      $codepoint = $byte & 0x01;
+    } else { // 11111110 and 11111111, hex FE-FF, are not allowed
+      return NeologismTurtleSerializer::error_character;
+    }
+    // Verify correct number of continuation bytes (0x80 to 0xBF)
+    $length = strlen($encoded_character);
+    if ($length < $bytes) { // not enough continuation bytes
+      return NeologismTurtleSerializer::error_character;
+    }
+    if ($length > $bytes) { // Too many continuation bytes -- show each as one error
+      $rest = str_repeat(NeologismTurtleSerializer::error_character, $length - $bytes);
+    } else {
+      $rest = '';
+    }
+    // Calculate Unicode codepoints from the bytes
+    for ($i = 1; $i < $bytes; $i++) {
+      // Loop over the additional bytes (0x80-0xBF, 10xxxxxx)
+      // Add their lowest six bits to the end of the codepoint
+      $byte = ord($encoded_character[$i]);
+      $codepoint = ($codepoint << 6) | ($byte & 0x3F);
+    }
+    // Check for overlong encoding (character is encoded as more bytes than
+    // necessary, this must be rejected by a safe UTF-8 decoder)
+    if (($bytes == 2 && $codepoint <= 0x7F) ||
+      ($bytes == 3 && $codepoint <= 0x7FF) ||
+      ($bytes == 4 && $codepoint <= 0xFFFF) ||
+      ($bytes == 5 && $codepoint <= 0x1FFFFF) ||
+      ($bytes == 6 && $codepoint <= 0x3FFFFF)) {
+      return NeologismTurtleSerializer::error_character . $rest;
+    }
+    // Check for UTF-16 surrogates, which must not be used in UTF-8
+    if ($codepoint >= 0xD800 && $codepoint <= 0xDFFF) {
+      return NeologismTurtleSerializer::error_character . $rest;
+    }
+    // Misc. illegal code positions
+    if ($codepoint == 0xFFFE || $codepoint == 0xFFFF) {
+      return NeologismTurtleSerializer::error_character . $rest;
+    }
+    if ($codepoint <= 0xFFFF) {
+      // 0x0100-0xFFFF, encode as \uXXXX
+//      return "\\u" . sprintf("%04X", $codepoint) . $rest;
+// Modified for Turtle
+      return substr($encoded_character, 0, $bytes) . $rest;
+    }
+    if ($codepoint <= 0x10FFFF) {
+      // 0x10000-0x10FFFF, encode as \UXXXXXXXX
+//      return "\\U" . sprintf("%08X", $codepoint) . $rest;
+// Modified for Turtle
+      return substr($encoded_character, 0, $bytes) . $rest;
+    }
+    // Unicode codepoint above 0x10FFFF, no characters have been assigned
+    // to those codepoints
+    return NeologismTurtleSerializer::error_character . $rest;
+  }
+}
